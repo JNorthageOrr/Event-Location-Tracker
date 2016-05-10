5dc0d7c20aecf921d2b1fd8a3ab6e85eb209bba8
Justin NORR <jnorthageorr@gmail.com>
2016-05-10 17:49:51 -0400

    adds campaign ID to images

--- alumnum/db/schema.rb
+++ alumnum/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510175644) do
+ActiveRecord::Schema.define(version: 20160510214851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 20160510175644) do
     t.datetime "updated_at",          null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "campaign_id"
   end
+
+  add_index "images", ["campaign_id"], name: "index_images_on_campaign_id", using: :btree
 
   create_table "profiles", force: :cascade do |t|
     t.string   "headline"
@@ -98,5 +101,6 @@ ActiveRecord::Schema.define(version: 20160510175644) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "images", "campaigns"
   add_foreign_key "profiles", "users"
 end
